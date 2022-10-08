e71c401503b24a586273ff362c8ebd14e81d0a68
Thu Juli <thujuli037@gmail.com>
2022-10-08 12:16:33 +0800

    restore fish config and plugin

---
+++ config/fish/config.fish
@@ -0,0 +1,12 @@
+starship init fish | source
+
+alias ls="exa --icons"
+alias lt="exa --tree --level=2"
+
+alias cp="cp -iv"
+alias mv="mv -iv"
+alias rm="trash-put"
+
+alias cat="bat"
+
+alias less="less -i"
